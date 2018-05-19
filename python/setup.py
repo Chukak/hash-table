232b3a78a485ae802691fc6504e384e29f60bda0
Chukak <chukak12@gmail.com>
2018-05-19 21:46:53 +0300

    updated setup script

--- python/setup.py
+++ python/setup.py
@@ -1,15 +1,35 @@
+import sys
 from distutils.core import setup, Extension
 
-setup(
-    name="hashtable",
-    version="1.0",
-    ext_modules=[
-        Extension(
-            "hashtable",
-            ["python.c", "../hashtable.c"],
-            extra_compile_args=[
-                "-g", "-std=c11", "-Werror", "-Wall", "-D_DEFAULT_SOURCE"
-            ])
-    ],
-    py_modules=["hashtable"],
-)
+
+if __name__ == "__main__":
+    paths = []
+    if "--include-paths" not in sys.argv:
+        one, two = sys.version_info[0], sys.version_info[1]
+        paths = [
+            "/usr/include/python" + str(one) + "." + str(two),
+            "/usr/local/include/python" + str(one) + "." + str(two),
+            "/usr/lib/python" + str(one) + "." + str(two),
+            "/usr/local/lib/python" + str(one) + "." + str(two),
+
+        ]
+    else:
+        index = sys.argv.index("--include-paths")
+        sys.argv.pop(index)
+        paths = [sys.argv.pop(index)]
+    setup(
+        name="hashtable",
+        version="1.0",
+        description="THe hash table, writen in C.",
+        url="https://github.com/Chukak/hash-table",
+        ext_modules=[
+            Extension(
+                "hashtable",
+                ["python.c", "../hashtable.c"],
+                include_dirs=paths,
+                extra_compile_args=[
+                    "-g", "-std=c11", "-Werror", "-Wall", "-D_DEFAULT_SOURCE"
+                ])
+        ],
+        py_modules=["hashtable"],
+    )
