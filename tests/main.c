48d578e892f932eb36088bfc59b2bcc49c3d89a7
Chukak <chukak12@gmail.com>
2018-06-29 16:21:53 +0300

    added the `main.c` file

---
+++ tests/main.c
@@ -0,0 +1,16 @@
+/* 
+    See hashtable.h for more info about struct hash_table type. 
+*/
+
+#include "tests.h"
+
+
+
+// main func
+int main(int argc, char** argv) {
+    if (run_tests()) {
+        return 1;
+    }
+    return 0;
+}
+
